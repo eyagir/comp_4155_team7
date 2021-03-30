35f579e0683aba47147cebedb78eec51cb3d559b
briggsy007 <hunter.briggs@outlook.com>
2021-03-30 18:01:46 -0400

    updated pom to change name

--- src/main/java/com/jh/GroupChatApp/controllers/GroupChatDataController.java
+++ src/main/java/com/jh/GroupChatApp/controllers/GroupChatDataController.java
@@ -1,6 +1,5 @@
 package com.jh.GroupChatApp.controllers;
 
-//import com.jh.covidapp.services.GroupChatDataService;
 
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
