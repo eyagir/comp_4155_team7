f2f97bd9abf57d16e63f39d516c30f85a1dcf8cd
Chase Scallion <scallionchase@gmail.com>
2021-04-28 22:11:51 -0400

    added delete funcionality to user posts

--- src/main/java/com/jh/GroupChatApp/controller/PostController.java
+++ src/main/java/com/jh/GroupChatApp/controller/PostController.java
@@ -1,12 +1,12 @@
 package com.jh.GroupChatApp.controller;
 
 import com.jh.GroupChatApp.model.Post;
+import com.jh.GroupChatApp.repository.PostRepository;
 import com.jh.GroupChatApp.repository.UserRepository;
 import com.jh.GroupChatApp.serviceInterface.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.sql.Timestamp;
@@ -22,11 +22,21 @@ public class PostController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepo;
+
     @PostMapping("/new")
     public String newPostIndex(Principal principal, Post post) {
         post.setUser(userRepository.findByUsername(principal.getName()));
         post.setCreatedAt(new Timestamp(new Date().getTime()));
         postService.createPost(post);
+        return "redirect:/account";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deletePostIndex(@PathVariable("id") int id) {
+        Post post = postService.getPostById(id);
+        postService.deletePost(post);
         return "redirect:/account";
     }
 }
